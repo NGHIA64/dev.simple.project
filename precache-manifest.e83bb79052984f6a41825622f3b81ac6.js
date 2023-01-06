25b262e8c58ae0fe5c02168d4d97900205650ecb
nghia64 <nguyentrongn168@gmail.com>
2023-01-06 17:27:52 +0700

    Updates

---
+++ precache-manifest.e83bb79052984f6a41825622f3b81ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3769472ad7aa00c2216d",
+    "url": "./static/css/main.66414a9b.chunk.css"
+  },
+  {
+    "revision": "3769472ad7aa00c2216d",
+    "url": "./static/js/main.3769472a.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "./static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "569b01859d1f23465362",
+    "url": "./static/js/2.569b0185.chunk.js"
+  },
+  {
+    "revision": "e698f5cb078272999522",
+    "url": "./static/js/runtime~main.e698f5cb.js"
+  },
+  {
+    "revision": "fa31a55c0877b2a416f26e0ee3ab5f32",
+    "url": "./index.html"
+  }
+];
